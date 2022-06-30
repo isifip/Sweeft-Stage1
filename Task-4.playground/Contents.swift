d2ef2a166aaebc73a6713e5af4e93a46aadae708
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-07-01 01:10:38 +0400

    + Task-4(solution)

--- Task-4.playground/Contents.swift
+++ Task-4.playground/Contents.swift
@@ -2,3 +2,21 @@ import Cocoa
 
 //MARK: -->  დავალება 4
 // მოცემულობა: მოცემულია String რომელიც შედგება „(„ და „)“ ელემენტებისგან. დაწერეთ ფუნქცია რომელიც აბრუნებს ფრჩხილები არის თუ არა მათემატიკურად სწორად დასმული.
+
+func checkParentheses(_ string: String) -> Bool {
+    let pair: [Character: Character] = ["(": ")"]
+    var stack: [Character] = []
+    for char in string {
+        if let match = pair[char] {
+            stack.append(match)
+        } else if stack.last == char {
+            stack.popLast()
+        } else {
+            return false
+        }
+    }
+    return stack.isEmpty
+}
+
+print(checkParentheses("()"))
+print(checkParentheses("(()("))
