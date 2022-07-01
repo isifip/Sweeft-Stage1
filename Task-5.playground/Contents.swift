99c089b9a1b036de30f4d08e8e5d0d1ef0889818
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-07-02 01:31:21 +0400

    Task-5 solution

--- Task-5.playground/Contents.swift
+++ Task-5.playground/Contents.swift
@@ -2,3 +2,27 @@ import Cocoa
 
 // MARK: --> დავალება 5
 // მოცემულობა: გვაქვს n სართულიანი კიბე, ერთ მოქმედებაში შეგვიძლია ავიდეთ 1 ან 2 საფეხურით. დაწერეთ ფუნქცია რომელიც დაითვლის n სართულზე ასვლის ვარიანტების რაოდენობას.
+
+
+func climbStairs(_ step: Int) -> Int {
+    if step < 0 {
+        return 0
+    }
+    if step == 0 || step == 1 {
+        return 1
+    }
+    
+    var previous = 0
+    var post = 1
+    var total = 0
+    
+    for _ in 1...step {
+        total = previous + post
+        previous = post
+        post = total
+    }
+    return total
+}
+
+climbStairs(5)
+climbStairs(4)
